da6c472fec96cc6bd1c367a0530067227333f7d6
techemmydev <gabrielemmanuelmary12345@gmail.com>
2025-04-25 17:34:31 +0100

    added privacy policy

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,14 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// i have my custom domain now
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://eventurehall.com"],
+    credentials: true,
+  })
+);
 app.use(cookieParser()); // allows us to parse incoming cookies from the client
 
 // Routes
